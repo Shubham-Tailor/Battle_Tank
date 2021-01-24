06448bcc41399079c5a6cce538aeed6bc494af5b
shubham tailor <shubhamtaylor9310@outlook.com>
2021-01-24 23:51:53 +0530

    Minor Updates, created some classes changed the "Parents" of the Blueprints

---
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay called through Tank cpp"));
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+	}
+	else
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possesing %s"), (*ControlledTank->GetName()));
+}
